b0d84595fc909b6b7711169e3073e6084934bedc
Petar <psimidzija@gmail.com>
2025-01-24 20:20:22 -0500

    Implement Embedding and PositionalEncoding

--- src/model.py
+++ src/model.py
@@ -1,6 +1,7 @@
 # Standard library
 
 # Third-party
+import numpy as np
 import torch
 import torch.nn as nn
 from torch import Tensor
@@ -12,7 +13,7 @@ from torch import Tensor
         
 #         # Layers
 #         self.embed = Embedding(vocab_size, d_model)
-#         self.pe = PositionalEncoding(d_model, max_wavelen)
+#         self.pe = PositionalEncoding(d_model, max_seq_len)
 #         self.dropout = Dropout(p_drop)
 #         self.blocks = nn.Sequential([Block(d_model, n_heads, p_drop)] 
 #                                     for _ in range(n_blocks))
@@ -52,13 +53,36 @@ class Embedding(nn.Module):
         self.d_model = d_model
 
         # Embedding tensor
-        k = 1 / torch.sqrt(d_model)
-        self.emb = nn.Parameter(k * (2 * torch.randn(vocab_size, d_model) - 1))
+        self.weight = nn.Parameter(torch.randn(vocab_size, d_model))
 
     def forward(self, x: Tensor) -> Tensor:
-        return self.emb[x]
+        return self.weight[x]
 
 
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model: int, max_seq_len: int) -> None:
+        if d_model % 2 != 0:
+            raise ValueError(f'd_model must be even but got {d_model}')
+        super().__init__()
+
+        # Params
+        self.d_model = d_model
+        self.max_seq_len = max_seq_len
+
+        # PE tensor
+        ps = torch.arange(0, max_seq_len)[:, None]
+        ds = torch.arange(0, d_model, 2)
+        wavelen = max_seq_len ** (ds / d_model)
+        weight = torch.zeros(max_seq_len, d_model)
+        weight[:, 0::2] = torch.sin(ps / wavelen)
+        weight[:, 1::2] = torch.cos(ps / wavelen)
+        
+        # Register buffer
+        self.register_buffer('weight', weight)
+
+    def forward(self, x: Tensor) -> Tensor:
+        seq_len = x.shape[-2]
+        return x + self.weight[..., :seq_len, :]
 
 
 
--- tests/test_model.py
+++ tests/test_model.py
@@ -7,8 +7,8 @@ import torch
 
 # Local
 ROOT = Path(__file__).resolve().parent.parent
-sys.path.append(ROOT)
-from model import Embedding
+sys.path.append(str(ROOT.absolute()/'src'))
+from model import (Embedding, PositionalEncoding)
 
 
 def test_Embedding():
@@ -17,8 +17,17 @@ def test_Embedding():
     batch_size = 3
 
     embed = Embedding(vocab_size, d_model)
-    x = torch.randint(0, vocab_size, (batch_size, 1))
+    x = torch.randint(0, vocab_size, (batch_size, ))
 
     assert embed(x).shape == torch.Size([batch_size, d_model])
 
+def test_PositionalEncoding():
+    max_seq_len = 9
+    seq_len = 7
+    d_model = 6
+    batch_size = 3
 
+    pe = PositionalEncoding(d_model, max_seq_len)
+    x = torch.rand(batch_size, seq_len, d_model)
+
+    assert pe(x).shape == x.shape
