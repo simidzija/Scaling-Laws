b13f0d25f6c3c9994a67f95ff7c74423742e1a04
Petar <psimidzija@gmail.com>
2025-01-27 20:17:43 -0500

    Implement tranformer

--- src/model.py
+++ src/model.py
@@ -1,4 +1,5 @@
 # Standard library
+from typing import Iterable, Optional
 
 # Third-party
 import numpy as np
@@ -7,42 +8,149 @@ import torch.nn as nn
 from torch import Tensor
 
 
-# class Transformer(nn.Module):
-#     def __init__(self):
-#         super().__init__()
+class Transformer(nn.Module):
+    def __init__(self, 
+                 vocab_size: int,
+                 d_model: int,
+                 max_seq_len: int,
+                 n_heads: int,
+                 n_blocks: int,
+                 p_drop: float=0.0) -> None:
+        super().__init__()
         
-#         # Layers
-#         self.embed = Embedding(vocab_size, d_model)
-#         self.pe = PositionalEncoding(d_model, max_seq_len)
-#         self.dropout = Dropout(p_drop)
-#         self.blocks = nn.Sequential([Block(d_model, n_heads, p_drop)] 
-#                                     for _ in range(n_blocks))
-#         self.ln = LayerNorm(d_model)
-#         self.deembed = DeEmbedding(d_model, vocab_size)
+        # Layers
+        self.embed = Embedding(vocab_size, d_model)
+        self.pe = PositionalEncoding(d_model, max_seq_len)
+        self.dropout = Dropout(p_drop)
+        self.blocks = nn.Sequential(*[Block(d_model, n_heads, p_drop) 
+                                      for _ in range(n_blocks)])
+        self.ln = LayerNorm(d_model)
+        self.deembed = nn.Linear(d_model, vocab_size)
 
-#     def forward(self, x: Tensor) -> Tensor:
-#         # Embedding
-#         x = self.embed(x)
+    def forward(self, x: Tensor) -> Tensor:
+        # Embedding
+        x = self.embed(x)
 
-#         # Positional Encoding
-#         x = self.pe(x)
+        # Positional Encoding
+        x = self.pe(x)
 
-#         # Dropout
-#         x = self.dropout(x)
+        # Dropout
+        x = self.dropout(x)
 
-#         # Transformer Blocks
-#         x = self.blocks(x)
+        # Transformer Blocks
+        x = self.blocks(x)
 
-#         # Layer Norm
-#         x = self.ln(x)
+        # Layer Norm
+        x = self.ln(x)
 
-#         # De-embedding
-#         x = self.deembed(x)
+        # De-embedding
+        x = self.deembed(x)
 
-#         return x
+        return x
 
 
 ##################################  Layers  ###################################
+
+class Block(nn.Module):
+    def __init__(self, d_model: int, n_heads: int, p_drop: float) -> None:
+        super().__init__()
+
+        # Params
+        self.d_model = d_model
+        self.n_heads = n_heads
+        self.p_drop = p_drop
+
+        # Layers
+        self.ln_mha = LayerNorm(d_model)
+        self.ln_ff = LayerNorm(d_model)
+        self.dropout = Dropout(p_drop)
+        self.mha = MultiheadAttention(d_model, n_heads, p_drop)
+        self.ff = FeedForward(d_model)
+
+    def forward(self, x: Tensor) -> Tensor:
+        # Multi-head attention
+        y = self.ln_mha(x)
+        y = self.mha(x)
+        y = self.dropout(x)
+        y = y + x
+
+        # Feed Forward
+        x = y
+        y = self.ln_ff(x)
+        y = self.ff(x)
+        y = self.dropout(x) 
+        y = y + x
+
+        return y
+
+class MultiheadAttention(nn.Module):
+    def __init__(self, d_model: int, n_heads: int, p_drop: int) -> None:
+        super().__init__()
+
+        # Params
+        if d_model % n_heads != 0:
+            raise ValueError(f'n_heads ({n_heads}) does not divide d_model {d_model}.')
+        self.d_model = d_model
+        self.n_heads = n_heads
+        self.head_size = d_model // n_heads
+        self.p_drop = p_drop
+
+        # Layers tensors
+        self.wqkv = nn.Linear(d_model, 3 * d_model)
+        self.wo = nn.Linear(d_model, d_model)
+        self.softmax = nn.Softmax(-1)
+        self.dropout = Dropout(p_drop)
+    
+    def forward(self, x: Tensor, mask: Optional[Tensor]=None) -> Tensor:
+        # Batching
+        batched = x.ndim == 3
+        if not batched:
+            x = x.unsqueeze(0)
+
+        # Dimeansion sizes
+        b = x.shape[0] if x.ndim == 3 else 1  # batch
+        s = x.shape[-2]  # sequence
+        h = self.n_heads  # head
+        f = self.head_size  # feature
+
+        # Q, K, V
+        q, k, v = self.wqkv(x).chunk(3, dim=-1)
+        q  = q.reshape(b, s, h, f).permute(0, 2, 1, 3)  # (b, h, s, f)
+        kT = k.reshape(b, s, h, f).permute(0, 2, 3, 1)  # (b, h, f, s)
+        v  = v.reshape(b, s, h, f).permute(0, 2, 1, 3)  # (b, h, s, f)
+
+        # Attention
+        mask = torch.full((s,s), -torch.inf).triu(1) if mask is None else mask
+        scores = q @ kT / np.sqrt(f) + mask
+        weights = self.softmax(scores)
+        weights = self.dropout(weights)
+        superpos = weights @ v  # (b, h, s, f)
+
+        # Linear
+        out = self.wo(superpos.permute(0, 2, 1, 3).reshape(b, s, h * f))
+
+        # Return
+        if batched:
+            return out
+        else:
+            return out.squeeze(0)
+
+
+class FeedForward(nn.Module):
+    def __init__(self, d_model: int) -> None:
+        super().__init__()
+
+        # Params
+        self.d_model = d_model
+
+        # Layers
+        self.layers = nn.Sequential(nn.Linear(d_model, 4 * d_model),
+                                    nn.ReLU(),
+                                    nn.Linear(4 * d_model, d_model))
+
+    def forward(self, x: Tensor) -> Tensor:
+        return self.layers(x)
+
 
 class Embedding(nn.Module):
     def __init__(self, vocab_size: int, d_model: int) -> None:
@@ -53,10 +161,10 @@ class Embedding(nn.Module):
         self.d_model = d_model
 
         # Embedding tensor
-        self.weight = nn.Parameter(torch.randn(vocab_size, d_model))
+        self.emb = nn.Parameter(torch.randn(vocab_size, d_model))
 
     def forward(self, x: Tensor) -> Tensor:
-        return self.weight[x]
+        return self.emb[x]
 
 
 class PositionalEncoding(nn.Module):
@@ -73,16 +181,59 @@ class PositionalEncoding(nn.Module):
         ps = torch.arange(0, max_seq_len)[:, None]
         ds = torch.arange(0, d_model, 2)
         wavelen = max_seq_len ** (ds / d_model)
-        weight = torch.zeros(max_seq_len, d_model)
-        weight[:, 0::2] = torch.sin(ps / wavelen)
-        weight[:, 1::2] = torch.cos(ps / wavelen)
+        pe = torch.empty(max_seq_len, d_model)
+        pe[:, 0::2] = torch.sin(ps / wavelen)
+        pe[:, 1::2] = torch.cos(ps / wavelen)
         
         # Register buffer
-        self.register_buffer('weight', weight)
+        self.register_buffer('pe', pe)
 
     def forward(self, x: Tensor) -> Tensor:
         seq_len = x.shape[-2]
-        return x + self.weight[..., :seq_len, :]
+        if seq_len > self.max_seq_len:
+            raise ValueError(f'seq_len ({seq_len}) is larger than max_seq_len ({self.max_seq_len}).')
+        return x + self.pe[..., :seq_len, :]
 
 
+class Dropout(nn.Module):
+    def __init__(self, p_drop: float) -> None:
+        super().__init__()
 
+        # Params
+        self. p_drop = p_drop
+
+    def forward(self, x: Tensor) -> Tensor:
+        if self.training:
+            mask = torch.rand_like(x) < self.p_drop
+            return x.masked_fill(mask, 0) / (1 - self.p_drop)
+        else:
+            return x
+
+class LayerNorm(nn.Module):
+    def __init__(self, normalized_shape: int | Iterable[int], 
+                 eps: float=0.00001) -> None:
+        super().__init__()
+
+        # Params
+        self.normalized_shape = (normalized_shape, ) if isinstance(normalized_shape, int) else normalized_shape 
+        self.eps = eps
+        self.n_dims = len(self.normalized_shape)
+        self.dims = list(range(-self.n_dims, 0))
+
+        # Gain and bias
+        self.gain = nn.Parameter(torch.ones(normalized_shape))
+        self.bias = nn.Parameter(torch.zeros(normalized_shape))
+    
+    def forward(self, x: Tensor) -> Tensor:
+        if x.shape[-self.n_dims:] != torch.Size(self.normalized_shape):
+            raise ValueError(f'Expected last {self.n_dims} dims of x to have shape {self.normalized_shape} but got {x.shape[-self.n_dims:]}.')
+
+        mean = torch.mean(x, dim=self.dims, keepdim=True)
+        var = torch.var(x, dim=self.dims, unbiased=False, keepdim=True)
+
+        return ((x - mean) / torch.sqrt(var + self.eps)) * self.gain + self.bias
+
+
+        
+
+        
--- tests/test_model.py
+++ tests/test_model.py
@@ -4,11 +4,19 @@ from pathlib import Path
 
 # Third-party
 import torch
+from torch import nn
 
 # Local
 ROOT = Path(__file__).resolve().parent.parent
 sys.path.append(str(ROOT.absolute()/'src'))
-from model import (Embedding, PositionalEncoding)
+from model import (Block, 
+                   Dropout, 
+                   Embedding, 
+                   FeedForward, 
+                   LayerNorm,
+                   MultiheadAttention,
+                   PositionalEncoding,
+                   Transformer)
 
 
 def test_Embedding():
@@ -31,3 +39,107 @@ def test_PositionalEncoding():
     x = torch.rand(batch_size, seq_len, d_model)
 
     assert pe(x).shape == x.shape
+
+def test_Dropout():
+    p_drop = 0.9
+    dropout = Dropout(p_drop)
+    N = 10000
+    x = torch.rand(N)
+
+    # training
+    dropout.train()
+    y = dropout(x)
+    avg_x = torch.mean(x)
+    avg_y = torch.mean(y)
+    n_zeros = torch.sum(y == 0, dtype=torch.float)
+    n_zeros_expected = torch.tensor(N * p_drop)
+
+    assert torch.allclose(avg_x, avg_y, rtol=0.1) # should be close
+    assert torch.allclose(n_zeros, n_zeros_expected, rtol=0.1)
+
+    # inference
+    dropout.eval()
+    y = dropout(x)
+
+    assert torch.equal(x, y)
+
+def test_LayerNorm():
+    x = torch.rand([3,4,5])
+    normalized_shape = [4,5]
+    eps = 0.1
+    ln = LayerNorm(normalized_shape, eps=eps)
+    ln_torch = nn.LayerNorm(normalized_shape, eps=eps)
+
+    assert torch.allclose(ln(x), ln_torch(x), rtol=0.001)
+
+def test_MultiheadAttention():
+    batch_size = 3
+    seq_len = 7
+    d_model = 6
+    n_heads = 2
+    p_drop = 0.1
+
+    mha = MultiheadAttention(d_model, n_heads, p_drop)
+    x1 = torch.rand(batch_size, seq_len, d_model)
+    assert mha(x1).shape == x1.shape
+
+    # check attention mechanism is causal
+    mha.eval()  # no dropout
+
+    # modify tokens >= i
+    i = 3
+    x2 = x1.clone()
+    x2[:, i:, :].uniform_()
+
+    # mha should give same outputs for tokens < i
+    out1 = mha(x1)
+    out2 = mha(x2)
+    assert torch.allclose(out1[:, :i, :], out2[:, :i, :])
+
+    # and different outputs for tokens >= i
+    assert not torch.any(torch.isclose(out1[:, i:, :], out2[:, i:, :]))
+
+test_MultiheadAttention()
+
+def test_FeedForward():
+    batch_size = 3
+    seq_len = 5
+    d_model = 7
+    
+    x = torch.rand(batch_size, seq_len, d_model)
+    ff = FeedForward(d_model)
+
+    assert ff(x).shape == x.shape
+
+def test_Block():
+    d_model = 6
+    n_heads = 2
+    p_drop = 0.1
+    batch_size = 3
+    seq_len = 5
+
+    block = Block(d_model, n_heads, p_drop)
+    x = torch.rand(batch_size, seq_len, d_model)
+
+    assert block(x).shape == x.shape
+
+def test_Transformer():
+    vocab_size = 17
+    d_model = 6
+    max_seq_len = 13
+    n_heads = 2
+    n_blocks = 7
+    p_drop = 0.1
+    batch_size = 9
+    seq_len = 11
+
+    transformer = Transformer(vocab_size=vocab_size,
+                              d_model=d_model,
+                              max_seq_len=max_seq_len,
+                              n_heads=n_heads,
+                              n_blocks=n_blocks,
+                              p_drop=p_drop)
+
+    x = torch.randint(low=0, high=vocab_size, size=(batch_size, seq_len))
+
+    assert transformer(x).shape == torch.Size([batch_size, seq_len, vocab_size])
