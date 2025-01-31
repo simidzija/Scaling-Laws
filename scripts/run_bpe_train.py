52b84de536caac93333682a4f3a3afe97ea91205
Petar <psimidzija@gmail.com>
2025-01-30 19:41:32 -0500

    Train BPE tokenizer

--- scripts/run_bpe_train.py
+++ scripts/run_bpe_train.py
@@ -11,8 +11,9 @@ from bpe_train import train
 
 
 if __name__ == '__main__':
-    corpus_path = str(ROOT/'data/data.txt')
+    files = [str(ROOT/'data/train_data.txt'), 
+             str(ROOT/'data/test_data.txt')]
     tokenizer_path = str(ROOT/'data/tokenizer.json')
     vocab_size = 32000  # same as Chinchilla
 
-    train(corpus_path, tokenizer_path, vocab_size)
+    train(files, tokenizer_path, vocab_size)
--- src/bpe_train.py
+++ src/bpe_train.py
@@ -4,7 +4,7 @@ from tokenizers.models import BPE
 from tokenizers.pre_tokenizers import Whitespace
 from tokenizers.trainers import BpeTrainer
 
-def train(corpus_path: str, tokenizer_path: str, vocab_size: int) -> None:
+def train(files: list[str], tokenizer_path: str, vocab_size: int) -> None:
     # Initialize tokenizer
     tokenizer = Tokenizer(BPE(unk_token="[UNK]"))
     tokenizer.pre_tokenizer = Whitespace()
@@ -13,7 +13,7 @@ def train(corpus_path: str, tokenizer_path: str, vocab_size: int) -> None:
     trainer = BpeTrainer(vocab_size=vocab_size, special_tokens=["[UNK]"])
 
     # Train on corpus
-    tokenizer.train([corpus_path], trainer)
+    tokenizer.train(files, trainer)
 
     # Save
     tokenizer.save(tokenizer_path)
