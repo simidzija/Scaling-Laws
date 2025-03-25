f8dfad8a4dd662d8be54c99ff08f037defd3eb52
Petar <psimidzija@gmail.com>
2025-03-25 17:38:31 -0400

    Create script for training on Fibonacci data

--- src/train.py
+++ src/train.py
@@ -109,39 +109,3 @@ def train(model: Transformer,
     return losses
 
 
-if __name__ == '__main__':
-    with open(ROOT/'data/fibonacci/metadata.json', 'r') as f:
-        metadata = json.load(f)
-        vocab_size = metadata['max_int']
-        seq_len = metadata['seq_len']
-        train_path = metadata['train_path']
-        n_seeds = metadata['n_seeds']
-        max_int = metadata['max_int']
-
-        # can't predict seed tokens since they are random
-        irreducible_loss = (n_seeds / (seq_len - 1)) * np.log(max_int - 2)
-
-    
-    model = Transformer(vocab_size=vocab_size,
-                        d_model=64,
-                        max_seq_len=seq_len,
-                        n_heads=2,
-                        n_blocks=3)
-
-    losses = train(model=model,
-                   device='cpu',
-                   train_path=train_path,
-                   data_dtype=np.int16,
-                   n_batches=10000,
-                   batch_size=32,
-                   seq_len=seq_len,
-                   lr=0.001,
-                   print_period=10)
-
-    plt.plot(losses, label='train loss')
-    plt.axhline(irreducible_loss, color='k', ls='--', label='irreducible loss')
-    plt.xlabel('batch')
-    plt.yscale('log')
-    plt.title(f'{n_seeds}-digit Fibonacci')
-    plt.legend()
-    plt.show()
